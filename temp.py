b7323ef5c0b522b1de8ae631af61ce9c63be265c
Adam Crawford <adamcrawford253@gmail.com>
2023-11-30 11:47:38 -0800

    Further testing.

--- temp.py
+++ temp.py
@@ -5,5 +5,6 @@ latest = requests.get("https://pypi.org/pypi/eedl/json").json()["info"]["version
 import eedl
 
 version_number = eedl.__version__
-setupcfg_version = os.environ.get("GREP_RESULT")
+setupcfg_version = os.environ.get("SETUPCFG_VERSION")
+print(latest, setupcfg_version)
 assert version_number == setupcfg_version and version_number > latest
